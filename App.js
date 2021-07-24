07a2e7518c1413fb505bff0371b7d42409847429
Wisienek <frascowoolf@gmail.com>
2021-07-25 00:49:16 +0200

    ✨ new menu + icons

--- App.js
+++ App.js
@@ -1,47 +1,59 @@
 import React from 'react';
 import {
-   SafeAreaView,
    StatusBar,
-   useColorScheme,
 } from 'react-native';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { enableScreens } from 'react-native-screens';
+import { Icon } from "react-native-elements";
+import * as styles from "components/css";
 
 enableScreens();
 
+import AllToures from 'components/AllToures';
 import MainScreen from 'components/MainScreen';
 import Menu from 'components/Menu';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Colors = {
-   white: "#fff",
-   black: "#000",
-   light: "#C0C0C0",
-   lighter: "#DCDCDC",
-   dark: "#202020",
-   darker: "#101010"
+
+const icons = {
+   "Główna": {
+      name: "map",
+      type: "material"
+   },
+   "Wszystkie Miejsca" : {
+      name: "find-replace",
+      type: "materialIcons"
+   }
 }
+
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
-
    return (
       <NavigationContainer>
          <Drawer.Navigator 
             drawerPosition="right"
-            initialRouteName="Home"
+            initialRouteName="Główna"
             drawerType="front"
-            edgeWidth={ 200 }
+            edgeWidth={ 70 }
+            drawerContent={ Menu }
+            screenOptions={ ({ route }) => ({
+               drawerIcon: ({  }) => (
+                  <Icon name={ icons[ route.name ].name } type={ icons[ route.name ].type } size={ 40 } color={ "#EAEAEA" } />
+               )
+            })}
          >
-            <Drawer.Screen name="Home" component={ MainScreen } />
-            <Drawer.Screen name="Menu" component={ Menu } />
+            <Drawer.Screen name="Główna" component={ MainScreen } />
+            <Drawer.Screen name="Wszystkie Miejsca" component={ AllToures } />
          </Drawer.Navigator>
       </NavigationContainer>
    );
 };
+
+// options={{ drawerIcon: ( <Icon name="map" type="material" size={ 40 } color={ isDarkMode? "#3F3F3F": styles.StaticText.color } /> ) }} 
 
 export default App;
