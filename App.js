f714c4ef82e92bc422f0314187b1102315eed32d
Wisienek <frascowoolf@gmail.com>
2021-08-02 19:33:59 +0200

    ♻️ refactor

--- App.js
+++ App.js
@@ -11,11 +11,11 @@ import * as styles from "components/css";
 
 enableScreens();
 
-import AllToures from 'components/AllToures';
-import MainScreen from 'components/MainScreen';
-import Menu from 'components/Menu';
+import AllToures from './components/AllToures';
+import MainScreen from './components/MainScreen';
+import Menu from './components/Menu';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import LikedContext from './components/context/LikedContext';
 
 
 const icons = {
@@ -34,26 +34,29 @@ const Drawer = createDrawerNavigator();
 
 const App = () => {
    return (
-      <NavigationContainer>
-         <Drawer.Navigator 
-            drawerPosition="right"
-            initialRouteName="Główna"
-            drawerType="front"
-            edgeWidth={ 70 }
-            drawerContent={ Menu }
-            screenOptions={ ({ route }) => ({
-               drawerIcon: ({  }) => (
-                  <Icon name={ icons[ route.name ].name } type={ icons[ route.name ].type } size={ 40 } color={ "#EAEAEA" } />
-               )
-            })}
-         >
-            <Drawer.Screen name="Główna" component={ MainScreen } />
-            <Drawer.Screen name="Wszystkie Miejsca" component={ AllToures } />
-         </Drawer.Navigator>
-      </NavigationContainer>
+      <>
+         <LikedContext>
+            <NavigationContainer>
+               <Drawer.Navigator 
+                  drawerPosition="right"
+                  initialRouteName="Główna"
+                  drawerType="front"
+                  edgeWidth={ 70 }
+                  drawerContent={ Menu }
+                  screenOptions={ ({ route }) => ({
+                     drawerIcon: ({  }) => (
+                        <Icon name={ icons[ route.name ].name } type={ icons[ route.name ].type } size={ 40 } color={ "#EAEAEA" } />
+                     )
+                  })}
+               >
+                  <Drawer.Screen name="Główna" component={ MainScreen } />
+                  <Drawer.Screen name="Wszystkie Miejsca" component={ AllToures } />
+               </Drawer.Navigator>
+            </NavigationContainer>
+         </LikedContext>
+      </>
    );
 };
 
-// options={{ drawerIcon: ( <Icon name="map" type="material" size={ 40 } color={ isDarkMode? "#3F3F3F": styles.StaticText.color } /> ) }} 
 
 export default App;
